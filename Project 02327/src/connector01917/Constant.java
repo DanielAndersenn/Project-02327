43da00019c9f7167fd3e6fd11178471e0f95a1be
Rasmus Edvardsen <rasmus.edvardsen@hotmail.com>
2016-05-09 17:27:52 +0200

    produktion->dtu

--- Project 02327/src/connector01917/Constant.java
+++ Project 02327/src/connector01917/Constant.java
@@ -7,9 +7,9 @@ public abstract class Constant
 {
 	public static final String
 		server					= "80.71.140.75",  // database-serveren
-		database				=  "02327",  //"jdbcdatabase", // navnet paa din database = dit studienummer
-		username				= "WebApp", // dit brugernavn = dit studienummer 
-		password				= "WebApp123"; // dit password som du har valgt til din database
+		database				= "02327",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "DTU", // dit brugernavn = dit studienummer 
+		password				= "HejVen123"; // dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
